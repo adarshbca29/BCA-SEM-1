5caf28d16a32a16fbd855b92a8eaba0536b3e5f4
Adarsh Kumar <adarshbca29@gmail.com>
2026-06-27 08:22:45 +0530

    n-10-20.c
    A program to print natural number from 10 to 20 when the initial counter is initialized to be 0 .

---
+++ Loops/n-10-20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   // A program to print natural number from 10 to 20 when the initial counter is initialized to be 0 \n\n.
+    int n = 0 ;
+    while (n<=20)
+    { 
+        if(n>=10){ 
+            printf("%d\n",n);
+        }
+        n++ ;
+    }
+    return 0;
+}
